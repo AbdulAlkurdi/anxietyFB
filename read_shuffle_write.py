028c84b76aa15ba20c17d7ff3ac50827cd1f4042
Abdul Alkurdi <alkurdi2@illinois.edu>
2023-11-22 15:38:55 -0600

    late morning update

--- read_shuffle_write.py
+++ read_shuffle_write.py
@@ -7,16 +7,17 @@ import itertools
 import pandas as pd
 import logging
 import pickle
+from tqdm import tqdm
 now = datetime.now()
 #home= '/mnt/c/Users/alkurdi/Downloads/WESAD'
 home = 'D:/Users/alkurdi/data' #windows version
 #home = '/mnt/d/Users/alkurdi/data/'
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', filename= f'{home}/logs/{now.strftime("%H_%M_%S")}_{os.path.basename(__file__)[0:-3]}.log' )
-logging.getLogger().addHandler(logging.StreamHandler())
+#logging.getLogger().addHandler(logging.StreamHandler())
 
 subject_ids = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 17]
 snrs = [ 0.01, 0.05, 0.3]#,0.1, 0.15, 0.2,0.0001, 0.001, 0.4, 0.5, 0.6] 
-n_i = [5,6]#[0,1,2,3,4] # next is [ 6] [7, 8, 9] 
+n_i = [0,1,2,3,4, 5, 6,7, 8, 9] 
 done = itertools.product([0.1, 0.05,0.3], [0,1,3,4], subject_ids)#snrs , n_i, subject_ids, [factor])
 i_max = len(snrs)*len(subject_ids)*len(n_i) 
 
@@ -31,7 +32,7 @@ def read_shuffle_write(snr,  n_i, subject_id, factor = 5):
     load_ws_path = wesad_path + '/S'+str(subject_id) + '/S'+str(subject_id)+'.pkl'
     load_gn_path =  gn_path + sesh_path + '/S'+str(subject_id)+'.pkl'
     factor = factor
-    savePath = gn_path + sesh_path + f'/fixed_resampled140hz_S{subject_id}.pkl'
+    savePath = gn_path + sesh_path + f'/organized_S{subject_id}.pkl'
     if os.path.exists(f'{savePath}'):
         msg = savePath+'n_i: '+ str(n_i)+ '; snr: '+ str(snr)+ '; subject_id: '+str(subject_id)+ ' already exist.'
         logging.info(msg)
@@ -51,15 +52,15 @@ def read_shuffle_write(snr,  n_i, subject_id, factor = 5):
         wrist = {}; chest = {}
         new_len = int(len(ws_df['signal']['chest']['ECG'])/factor)
         logging.debug(f'{sesh_id} resampling and playing with chest')
-        fixed_gn_df['label'] = signal.resample(ws_df['label'],new_len)
-        chest['ACC'] = signal.resample(gn_df['ACC'],new_len) #gn_df['ACC'] #= #
-        chest['ECG'] = signal.resample(gn_df['ECG'],new_len) #= gn_df['ECG']
-        chest['EMG'] = signal.resample(gn_df['EMG_C'],int(new_len/100)+1)#= gn_df['EMG_C'] 
-        chest['EDA'] = signal.resample(gn_df['EDA_C'],int(new_len/100)+1)#= gn_df['EDA_C']
-        chest['Temp'] = signal.resample(gn_df['Temp_C'],int(new_len/100)+1)#= gn_df['Temp_C']
-        chest['Resp'] = signal.resample(gn_df['Resp_C'],new_len)#gn_df['Resp_C'] #
+        fixed_gn_df['label'] = ws_df['label'] #signal.resample(ws_df['label'],new_len)
+        chest['ACC'] = gn_df['ACC'] #gn_df['ACC'] #= #
+        chest['ECG'] = gn_df['ECG'] #= gn_df['ECG']
+        chest['EMG'] = gn_df['EMG_C'] #= signal.resample(gn_df['EMG_C'],int(new_len/100)+1)#
+        chest['EDA'] = gn_df['EDA_C']#signal.resample(gn_df['EDA_C'],int(new_len/100)+1)#
+        chest['Temp'] = gn_df['Temp_C']#= signal.resample(gn_df['Temp_C'],int(new_len/100)+1)#
+        chest['Resp'] = gn_df['Resp_C'] #signal.resample(gn_df['Resp_C'],new_len)#
         logging.debug(f'{sesh_id} resampling and playing with wrist')
-        wrist['ACC'] = signal.resample(gn_df['ACC'],new_len) #= gn_df['ACC']
+        wrist['ACC'] = gn_df['ACC']# =signal.resample(gn_df['ACC'],new_len) #
         wrist['BVP'] = gn_df['BVP'] # signal.resample(gn_df['BVP'],new_len) #= 
         wrist['EDA'] = gn_df['EDA'] #signal.resample(gn_df['EDA'],new_len) #= 
         wrist['TEMP'] = gn_df['TEMP'] #= signal.resample(gn_df['TEMP'],new_len) # 
@@ -77,12 +78,14 @@ def main():
     factor = 5 #reducing the bigger (ECG) sampling rate by a factor of 5 snr,  n_i, subject_id
     combo = itertools.product(snrs , n_i, subject_ids, [factor]) # itertools.product(list(range(n_samples)), snrs, subject_ids, factor)
     logging.debug(f'combo: {list[combo]}')
-    with concurrent.futures.ProcessPoolExecutor(max_workers=6) as executor:
-        futures = [executor.submit(read_shuffle_write, i[0], i[1], i[2], i[3]) for i in combo]
-        done, not_done = concurrent.futures.wait(futures, return_when='ALL_COMPLETED')
-        logging.debug(f'done assigning futures, now waiting for them to finish')
-        for future in done:
-            results = future.result()
+    with tqdm(total = i_max) as pbar:
+        with concurrent.futures.ProcessPoolExecutor(max_workers=6) as executor:
+            futures = [executor.submit(read_shuffle_write, i[0], i[1], i[2], i[3]) for i in combo]
+            done, not_done = concurrent.futures.wait(futures, return_when='ALL_COMPLETED')
+            logging.debug(f'done assigning futures, now waiting for them to finish')
+            for future in done:
+                results = future.result()
+                pbar.update(1)
     # we'll let each process write its own file
     #for savePath, df in results:
     #    pd.to_pickle(df, savePath)
